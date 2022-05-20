25205d7e4564ae419fabf2f16edb05e7b107dbb6
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-05-20 08:37:13 -0500

    changed buttons to selector

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,17 +5,34 @@ function onReady() {
     // add click listeners for all four math buttons here
     // better to have one listener for the class of buttons and then the function call will decide which is being clicked
 
-    $('.mathBtn').on('click', mathBtn);
+    // Below only need if doing multiple buttons, for now just doing a selector
+    // $('.mathBtn').on('click', mathBtn);
     $('#submitBtn').on('submit', submitMath)
 }
 
-function mathBtn(){
-    // decide which button was clicked and add that button to the object to send
-}
 
 function submitMath(evt) {
     // prevent form from reloading page
     evt.preventDefault();
     console.log('button works!')
-    // continue from here
+    
+    let mathInputs = {
+        numOne: Number($('#numOne').val()),
+        numTwo: Number($('#numTwo').val()),
+        operator: $('#operator').val()
+    }
+
+    $.ajax({
+        // create post to send data to server
+        url: '/calculate',
+        method: 'POST',
+        data: mathInputs,
+    }).then(() => {
+        console.log('POST /calculate working')
+
+        // call the GET
+        fetchMath()
+    }).catch((err) => {
+        console.log('error is', err)
+    })
 }
